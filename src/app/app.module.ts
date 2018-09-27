ca98ff10a898babf18c3292a9339687e583b4ffc
jvdboom <jvdboom@hotmail.com>
2018-09-27 10:33:03 +0200

    Working PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MenuComponent } from "./menu/menu.component";
+
+import { ButtonModule } from "primeng/button";
+import { SplitButtonModule } from "primeng/splitbutton";
+import { AccordionModule } from "primeng/accordion";
+import { MenuModule } from "primeng/menu";
+import { MenubarModule } from "primeng/menubar";
+import { InputTextModule } from "primeng/inputtext";
+import { HomeComponent } from "./components/home/home.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    SplitButtonModule,
+    InputTextModule,
+    AccordionModule,
+    MenuModule,
+    MenubarModule,
     AppRoutingModule
   ],
   providers: [],
